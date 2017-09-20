2925b4dc61c40aec7a6ca1a07e3ab3cff8d47cc6
mathijsWesterhof <mathijswesterhof0506@gmail.com>
2017-09-20 20:08:36 +0200

    the two most basic models

---
+++ app/Training.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Training extends Model
+{
+	
+	protected $fillable = ['name', 'color',]; 
+	protected $table    = 'training';
+    public $timestamps  = false;
+
+    public function colloquia()
+    {
+    	return $this->hasMany(Colloquium::class);
+    }
+
+}
