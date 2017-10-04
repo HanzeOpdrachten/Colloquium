f6390ff9cf0c94c549b3e5e5b9e4911c0b628613
Paul Bergmann <pauljbergmann@gmail.com>
2017-10-04 17:37:32 +0200

    Add route

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,7 @@ Route::get('/colloquia', 'ColloquiaController@index')->name('colloquia.index');
 Route::get('/colloquia/create', 'ColloquiaController@create')->name('colloquia.create');
 Route::post('/colloquia', 'ColloquiaController@store')->name('colloquia.store');
 Route::get('/colloquia/{colloquium}', 'ColloquiaController@show')->name('colloquia.show');
-Route::get('/colloquia/{colloquium}/edit', 'ColloquiaController@edit')->name('colloquia.edit');
-Route::patch('/colloquia/{colloquium}', 'ColloquiaController@update')->name('colloquia.update');
+Route::get('/colloquia/{token}/manage', 'ColloquiaController@manage')->name('colloquia.manage');
 
 /*
  * Authenticated routes.
@@ -42,4 +41,6 @@ Route::middleware('auth')->group(function() {
 
     Route::get('/colloquia/accept/{colloquium}', 'ColloquiaController@accept')->name('colloquia.accept');
     Route::get('/colloquia/decline/{colloquium}', 'ColloquiaController@decline')->name('colloquia.decline');
+    Route::get('/colloquia/{colloquium}/edit', 'ColloquiaController@edit')->name('colloquia.edit');
+    Route::patch('/colloquia/{colloquium}', 'ColloquiaController@update')->name('colloquia.update');
 });
