20f84f913c436300dfae5023494e016a97264b59
Michael Lanting <info@mlanting.nl>
2017-10-15 16:10:08 +0200

    Added SASS files for styling, did work on index page for more info on installing the SASS processor check slack / #general

---
+++ public/js/scripts.js
@@ -0,0 +1,37 @@
+jQuery(function(){
+  jQuery('#js-menu-toggle').click(function(){
+
+    jQuery(this).find('.hamburger__slice--top').toggleClass('hamburger__slice--top--active');
+    jQuery(this).find('.hamburger__slice--middle').toggleClass('hamburger__slice--middle--active');
+    jQuery(this).find('.hamburger__slice--bottom').toggleClass('hamburger__slice--bottom--active');
+
+    jQuery('body').toggleClass("menu-active");
+    jQuery('.navigation').toggleClass("navigation--active");
+    jQuery('.menu').toggleClass("menu--active");
+
+    return false;
+  });
+
+  jQuery('.menu__item > a').click(function () {
+    for (var i = 0; i < 3; i++) {
+      var position = ['top', 'middle', 'bottom'];
+
+      if (jQuery('.hamburger__slice--' + position[i]).hasClass('hamburger__slice--' + position[i] + '--active')) {
+        jQuery('.hamburger__slice--' + position[i]).removeClass('hamburger__slice--' + position[i] + '--active');
+      }
+    }
+
+    checkRemoveClass('body', 'menu-active');
+    checkRemoveClass('.navigation', 'navigation--active');
+    checkRemoveClass('.menu', 'menu--active');
+  });
+
+  function checkRemoveClass(targetEl, targetClass) {
+    if (jQuery(targetEl).hasClass(targetClass)) {
+      jQuery(targetEl).removeClass(targetClass);
+    }
+
+    return false;
+  }
+
+});
