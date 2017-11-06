a3811a0ddda8907befeedf053085a5b38cb296c8
reinvdwoerd <reinvanderwoerd@me.com>
2017-11-06 12:15:23 +0100

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/2.5.1/css/bootstrap-colorpicker.css" />
     <link rel="stylesheet" href="/css/theme.min.css">
-    <link rel="icon" href="/img/favicon.svg">
-
+    <link rel="icon" type="image/png" sizes="192x192"  href="/img/favicon.png">
   </head>
   <body>
     @if(!isset($noHeader))
